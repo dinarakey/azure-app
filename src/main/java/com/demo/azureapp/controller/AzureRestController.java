b3bff51325f96ae6a4174262273ea7cc10b4df99
Edgaras <edgarasvenzlauskas@gmail.com>
2023-07-07 19:08:33 +0300

    add upload feature

--- src/main/java/com/demo/azureapp/controller/AzureRestController.java
+++ src/main/java/com/demo/azureapp/controller/AzureRestController.java
@@ -1,11 +1,15 @@
 package com.demo.azureapp.controller;
 
+import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.models.BlobItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 @RestController
@@ -22,10 +26,27 @@ public class AzureRestController {
     @GetMapping("/blob")
     public String blobs() {
         ArrayList<String> list = new ArrayList<String>();
-        for (BlobItem i: container.listBlobs()
-             ) {
+        for (BlobItem i : container.listBlobs()
+        ) {
             list.add(i.getName());
         }
         return list.toString();
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return container.getBlobClient("No ithernet.txt").downloadContent().toString();
+    }
+
+    @PostMapping("/share")
+    public String share(MultipartFile file) throws IOException {
+        BlobClient client = container.getBlobClient(file.getOriginalFilename());
+        if (client.exists()) {
+            return file.getOriginalFilename() + " file is already exists!";
+        } else {
+            client.upload(file.getInputStream(), file.getSize());
+            return file.getOriginalFilename() + " file shared!";
+        }
+
     }
 }
