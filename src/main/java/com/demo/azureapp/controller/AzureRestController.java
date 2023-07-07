0efc5fe634bb8b24d013668afb74e7f3f8db0203
Edgaras <edgarasvenzlauskas@gmail.com>
2023-07-07 16:44:34 +0300

    add blob connection

--- src/main/java/com/demo/azureapp/controller/AzureRestController.java
+++ src/main/java/com/demo/azureapp/controller/AzureRestController.java
@@ -1,13 +1,31 @@
 package com.demo.azureapp.controller;
 
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.models.BlobItem;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class AzureRestController {
 
+    @Autowired
+    private BlobContainerClient container;
+
     @GetMapping("/hi")
-    public String greetings(){
+    public String greetings() {
         return "greetings!";
+    }
+
+    @GetMapping("/blob")
+    public String blobs() {
+        ArrayList<String> list = new ArrayList<String>();
+        for (BlobItem i: container.listBlobs()
+             ) {
+            list.add(i.getName());
+        }
+        return list.toString();
     }
 }
